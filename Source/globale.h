95a6c075df1765eca997e2b53084aa47102a3a1d
gregouar <gregouar@02226d48-ee4e-0410-85af-ca5f3f0fcfbc>
2012-03-19 11:18:06 +0000

    Move resize into sfml.cpp

--- Source/globale.h
+++ Source/globale.h
@@ -37,11 +37,7 @@ MySprite(const Texture& texture);
 
 MySprite(const Texture& texture, const IntRect& rectangle);*/
 
-void resize(float x, float y)
-{
-    setScale(x/(float)getLocalBounds().width  * ((getScale().x > 0) ? 1.f : -1.f),
-             y/(float)getLocalBounds().height * ((getScale().y > 0) ? 1.f : -1.f));
-}
+void resize(float x, float y);
 
 };
 
--- Source/sfml.cpp
+++ Source/sfml.cpp
@@ -1,6 +1,6 @@
 #include "globale.h"
 
-void MySprite::Resize(float x, float y)
+void MySprite::resize(float x, float y)
 {
     setScale(x/(float)getLocalBounds().width  * ((getScale().x > 0) ? 1.f : -1.f),
              y/(float)getLocalBounds().height * ((getScale().y > 0) ? 1.f : -1.f));
