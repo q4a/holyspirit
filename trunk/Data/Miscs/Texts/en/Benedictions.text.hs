cc89acc57b6bb309122f42d2dbbf2659716d5e42
ig0rk0 <ig0rk0@02226d48-ee4e-0410-85af-ca5f3f0fcfbc>
2012-03-21 08:18:29 +0000

    Recover old English Texts

---
+++ trunk/Data/Miscs/Texts/en/Benedictions.text.hs
@@ -0,0 +1,21 @@
+Increased efficiency : 
+Strength increased : 
+Dexterity increased : 
+Vitality increased : 
+Piety increased : 
+Charisma increased : 
+Health increased : 
+Faith increased : 
+Damages increased :
+Fire damages : 
+Faith damages : 
+Corrosion damages :
+Armor increased :
+Fire resistance :
+Faith resistance :
+Corrosion resistance :
+Damage by bleeding :
+Damage by burning:
+Damage by purification :
+Damage by poisoning :
+over
