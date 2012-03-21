c088734dddcf869f0a96713dfb10008c8bbd7d38
ig0rk0 <ig0rk0@02226d48-ee4e-0410-85af-ca5f3f0fcfbc>
2012-03-21 08:18:29 +0000

    Recover old English Texts

---
+++ Data/Miscs/Texts/en/Maps.text.hs
@@ -0,0 +1,13 @@
+Agrarian Lands
+Agrarian Lands
+Agrarian Lands
+Loading
+The Graveyard
+The Dungeon Of Fear
+Tutorial
+The One Thousand and One Traps Cavern
+Malemort Forest
+
+The Abbey
+
+Catacombs
---
+++ Data/Miscs/Texts/en/Menus.text.hs
@@ -0,0 +1,66 @@
+Return
+Save and exit
+Reinitialise the game and exit
+Health
+Faith
+Strength
+Dexterity
+Vitality
+Piety
+Charisma
+Damages
+Armor
+Points left
+Display/Hide map (M)
+Inventory (I)
+Quests (Q)
+Miracles (T)
+Menu (Escape)
+Equiped :
+Trader :
+Inventory :
+Damages : 
+Armor : 
+Required strength : 
+Required dexterity : 
+Required charisma : 
+Required vitality : 
+Required piety : 
+Price : 
+Faith cost : 
+Faith stock : 
+Health cost : 
+Health stock : 
+Items shortcuts
+Miracles shortcuts
+The Persecutor path
+The Protector path
+The Pious Path
+The Poisoner Path
+The Assassin Path
+The Seductress Path
+Next Level :
+Left-Click : Select / Right-Click : Learn
+You got a new level ! Click here to spend your points.
+Physic
+Fire
+Faith
+Corrosion
+Money
+Level
+Name
+Console (Tab)
+Only for : 
+Continue a game
+New game
+Options
+Exit
+Back
+Your name :
+Launch
+Free socket
+Toggle buttons
+Class
+Chest :
+<Incompatible>
+
