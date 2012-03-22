f20a8b4bf0e4edaee0061fc8ade3a929432430c5
ig0rk0 <ig0rk0@02226d48-ee4e-0410-85af-ca5f3f0fcfbc>
2012-03-22 14:01:20 +0000

    Fix symbol

--- trunk/Data/Miscs/Texts/en/Benedictions.text.hs
+++ trunk/Data/Miscs/Texts/en/Benedictions.text.hs
@@ -3,8 +3,8 @@ Damage by fire:
 Damage by faith: 
 Damage by corrosion: 
 Armor increased: 
-Resistance to fire: 
-Resistance à faith: 
+Fire resistance: 
+Faith resistance: 
 Corrosion resistance: 
 Damage per bleeding: 
 Damage by the burn: 
